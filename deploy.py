0861f9e3e787160a85df8111debd38945eb138d8
James Hetherington <jamespjh@gmail.com>
2014-10-21 10:49:40 +0100

    Add script to do deployment

---
+++ deploy.py
@@ -0,0 +1,13 @@
+# Modify the config files to the version used in deployment
+import yaml
+
+deployed_path="/training/engineering"
+
+plugin=yaml.load(open('dexyplugin.yaml'))
+plugin['reporter:supplementary']['root']=deployed_path
+yaml.dump(plugin,open('dexyplugin.yaml','w'))
+
+dexy=yaml.load(open('dexy.yaml'))
+dexy['**/slides.md|jinja|hd|jinja|pandoc|-reveal|resub|h'
+     ][2]['resub']['expressions'][2][1]=deployed_path
+yaml.dump(dexy,open('dexy.yaml','w'))
