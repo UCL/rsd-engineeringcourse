2a843fa29da97eb0516011e21436eced765d4145
James Hetherington <jamespjh@gmail.com>
2014-10-22 13:39:25 +0100

    Use non-interactive backend for matplotlib

--- session04/python/greengraph.py
+++ session04/python/greengraph.py
@@ -87,6 +87,8 @@ def location_sequence(start,end,steps):
 
 
 ### "save"
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 with open('green.png','w') as green:
     green.write(show_green_in_png(map_at(*london_location,zoom=10,satellite=True)))
