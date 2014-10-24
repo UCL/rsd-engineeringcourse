9a59b42a8705a9bf2e3fa4f42454f5cfc6c7eb46
James Hetherington <jamespjh@gmail.com>
2014-10-24 17:40:12 +0100

    Tweak deploy script to match new filter

--- deploy.py
+++ deploy.py
@@ -9,6 +9,6 @@ plugin['reporter:supplementary']['root']=deployed_path
 yaml.dump(plugin,open('dexyplugin.yaml','w'))
 
 dexy=yaml.load(open('dexy.yaml'))
-dexy['**/slides.md|jinja|hd|jinja|pandoc|-reveal|resub|h'
+dexy['**/slides.md|jinja|hd|jinja|resub|pandoc|-reveal|resub|h'
      ][2]['resub']['expressions'][2][1]=deployed_path
 yaml.dump(dexy,open('dexy.yaml','w'))
