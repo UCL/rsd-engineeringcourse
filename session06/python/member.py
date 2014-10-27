9dff541b359bab37cdfc60adfec1638c8168ac93
James Hetherington <jamespjh@gmail.com>
2014-10-27 18:42:26 +0000

    Hide future lessons again on master branch

--- session06/python/member.py
+++
@@ -1,6 +0,0 @@
-class MyClass(object):
-   def __init__(self):
-      self.member = "Value"
-
-my_object = MyClass()
-assert(my_object.member == "Value")
--- session06/python/method.py
+++
@@ -1,7 +0,0 @@
-class MyClass(object):
-   def someMethod(self, argument):
-      pass
-
-my_object=MyClass()
-value=5
-my_object.someMethod(value)
