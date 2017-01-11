b9a6ba3de6370bf77b1ba1d5fc69504b4d1b1098
James Hetherington <jamespjh@gmail.com>
2017-01-11 18:04:57 +0000

    Python 2 ification, still trying to fix latex bug

--- ch04packaging/greetings/setup.py
+++ ch04packaging/greetings/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "Greetings",
-    version = "0.1",
+    version = "0.1.0",
     packages = find_packages(exclude=['*test']),
     scripts = ['scripts/greet'],
     install_requires = ['argparse']
