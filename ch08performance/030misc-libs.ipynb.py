23037f4f89659da52b0e683fa1c71436345c4c52
Saransh Chopra <saransh0701@gmail.com>
2024-08-23 07:42:34 +0100

    Apply suggestions from code review
    Co-authored-by: David Pérez-Suárez <d.perez-suarez@ucl.ac.uk>

--- ch08performance/030misc-libs.ipynb.py
+++ ch08performance/030misc-libs.ipynb.py
@@ -24,10 +24,13 @@
 # 
 # We know that Python is an interpreted and not a compiled language, but is there a way
 # to compile Python code? There are a few libraries/frameworks that lets you
-# *just in time* or *ahead of time* compile Python code. Both the techniques allow
+# [*just in time*][wiki-jit] (JIT) or [*ahead of time*][wiki-aot] (AOT) compile Python code. Both the techniques allow
 # users to compile their Python code without using any explicit low level langauge's
 # bindings, but both the techniques are different from each other.
 # 
+# [wiki-jit]: https://en.wikipedia.org/wiki/Just-in-time_compilation
+# [wiki-aot]: https://en.wikipedia.org/wiki/Ahead-of-time_compilation
+#
 # Just in time compilers compile functions/methods at runtime whereas ahead of time
 # compilers compile the entire code before runtime. AOT can do much more compiler
 # optimizations than JIT, but AOT compilers produce huge binaries and that too only
@@ -62,9 +65,12 @@ from numba import jit, njit
 import numpy as np
 
 # %% [markdown]
-# We can mark a function to be JIT compiled by simply decorating it
+# We can mark a function to be JIT compiled by decorating it
 # with numba's `@jit`. The decorator takes a `nopython` argument that tells Numba to
 # enter the compilation mode and not fall back to usual Python execution.
+# 
+# Here, we are showing a usual python function, and one that's decorated. 
+# We don't need to duplicate and change its name when using numba, but we want to keep both of them here to compare their execution times.
 
 # %%
 def f(x):
@@ -170,7 +176,7 @@ data = [[mandel1(complex(x, y)) for x in xs] for y in ys]
 # The compiled code already beats our fastest NumPy implementation! It is not
 # necessary the compiled code will perform better than NumPy code, but it usually
 # gives performance gains for signifantly large computations. As always, it is
-# good to measure the perrformance to check if there are any gains.
+# good to measure the performance to check if there are any gains.
 
 # %% [markdown]
 # Let's try JITting our NumPy code.
@@ -182,7 +188,7 @@ def mandel_numpy(position,limit=50):
     diverged_at_count = np.zeros(position.shape)
     while limit > 0:
         limit -= 1
-        value = value**2+position
+        value = value**2 + position
         diverging = value * np.conj(value) > 4
         first_diverged_this_time = np.logical_and(diverging, diverged_at_count == 0)
         diverged_at_count[first_diverged_this_time] = limit
